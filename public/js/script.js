01820650c61cdd585d2f0b9ceb40e2c33039eeec
Priyojit Roy <priyojit.roy@cognizant.com>
2018-09-25 01:18:23 +0530

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,125 @@
-function sendMessage() {
+function sendMessage(event, socket) {
+	event.preventDefault();
+	const channel =  document.getElementById('channel').value;
+	const message =  document.getElementById('message').value;
+	const username = document.getElementById('username').value;
+	socket.emit('message', {
+		channel,
+		message,
+		username
+	});
+	let innerhtml= '';
+ 	const chatContainer = document.getElementById('chatContainer');
+ 	const messageElement = document.createElement('div');
+ 	messageElement.innerHTML = innerhtml +
+		`<div class="card sent-message">
+			<div class="card-body">
+				<p class="card-text">Me : ${message}</p>
+			</div>
+		
+		</div>`;
+	messageElement.className = 'col-12';
+	chatContainer.insertBefore(messageElement,chatContainer.childNodes[0]);
 }
 
-function joinChannel() {
+function joinChannel(event, socket) {
+	event.preventDefault();
+	const channel =  document.getElementById('newchannel').value;
+	let joinChannels = channel.split(',');
+	joinChannels.forEach(channel => {
+		socket.emit('joinChannel', {
+			channel: channel
+		});
+	});
 }
 
-function leaveChannel() {
+function leaveChannel(event, socket) {
+	event.preventDefault();
+	const channel =  document.getElementById('newchannel').value;
+	let joinChannels = channel.split(',');
+	joinChannels.forEach(channel => {
+		socket.emit('leaveChannel', {
+			channel: channel
+		});
+	});
 }
 
-function onWelcomeMessageReceived() {
+function onWelcomeMessageReceived(data) {
+	let innerhtml= '';
+	const chatContainer = document.getElementById('chatContainer');
+	const messageElement = document.createElement('div');
+	messageElement.innerHTML = innerhtml +
+		`<div class="col-12">
+			<div class="card received-message">
+				<div class="card-body">
+					<p class="card-text">System : ${data}</p>
+				</div>
+			</div>
+		</div>`;
+	chatContainer.appendChild(messageElement);
 }
 
-function onNewMessageReceived() {
+function addToChannelList(channel){
+	var dropdown = document.getElementById('channelsList');
+	let options = document.createElement('option');
+	options.value = channel.channel;
+	dropdown.appendChild(options);
 }
 
-function onAddedToNewChannelReceived() {
+function onNewMessageReceived(message) {
+	let innerhtml= '';
+	const chatContainer = document.getElementById('chatContainer');
+	const messageElement = document.createElement('div');
+	messageElement.innerHTML = innerhtml +
+		`<div class="card sent-message">
+			<div class="card-body">
+				<p class="card-text">${message.username}: ${message.message}</p>
+			</div>
+		</div>`;
+	messageElement.className = 'col-12';
+	chatContainer.insertBefore(messageElement, chatContainer.childNodes[0]);
 }
 
-function onRemovedFromChannelReceived() {
+function onRemovedFromChannelReceived(channel) {
+	const alertContainer = document.getElementById('alertContainer');
+	let innerhtml='';
+	const alertElement = document.createElement('div');
+	alertElement.innerHTML= innerhtml +
+		`<div class="alert alert-success alert-dismissible fade show" role="alert">
+			You are removed to <strong>${channel.Channel}</strong> successfully!
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>`;
+	alertContainer.appendChild(alertElement);
+	let chname = channel.Channel;
+	var dropdown = document.getElementById('channelsList');
+	let options = dropdown.getElementsByTagName('option');
+	for(var i=0; i<options.length; i = i + 1){
+		if(options[i].value == chname){
+			dropdown.removeChild(options[i]);
+		}
+	}
+	options.value = '';
+	dropdown.appendChild(options);
+}
+
+function alertMessage(channel){
+	const alertContainer = document.getElementById('alertContainer');
+	const alertElement = document.createElement('div');
+	alertElement.innerHTML =
+		`<div class="alert alert-success alert-dismissible fade show" role="alert">
+			You are added to <strong>${channel.channel}</strong> successfully!
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>`;
+	alertContainer.appendChild(alertElement);
+}
+
+function onAddedToNewChannelReceived(channel) {
+	alertMessage(channel);
+	addToChannelList(channel);
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 function bootstrapSocketServer(io) {
-	io.on('connection', () => {
+	io.on('connection', (socket) => {
+		socket.on('register', (data) => {
+			socket.emit('welcomeMessage', `${data.username}`);
+			data.channels.forEach(channel => {
+				socket.join(channel);
+				socket.emit('addedToChannel', {channel});
+			});
+			socket.on('joinChannel',(joinChannelData) => {
+				socket.join(joinChannelData.channel);
+				socket.emit('addedToChannel',{channel: joinChannelData.channel});
+			});
+			socket.on('leaveChannel',(removeChannelData) => {
+				socket.leave(removeChannelData.channel);
+				socket.emit('removedFromChannel',{channel: removeChannelData.channel});
+			});
+			socket.on('message',(data) => {
+				socket.broadcast.emit('newMessage',data);
+			});
+		});
 	});
 }
 
