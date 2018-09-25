4c0db16e9e7f2f788a205273b2d17b2cabf25b72
Ubuntu <ubuntu@ip-172-31-89-151.ec2.internal>
2018-09-25 07:10:37 +0000

    fixed hobbes error

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 function bootstrapSocketServer(io) {
 	io.on('connection', (socket) => {
 		socket.on('register', (data) => {
-			socket.emit('welcomeMessage', `${data.username}`);
-			data.channels.forEach(channel => {
-				socket.join(channel);
-				socket.emit('addedToChannel', {channel});
-			});
+			if(data.username !== null && data.channels !== null) {
+				const username = data.username;
+				const channels = data.channels;
+				socket.emit('welcomeMessage', `Welcome ${username}`);
+				channels.forEach(channel => {
+					socket.join(channel);
+					socket.emit('addedToChannel', {
+						channel: channel
+					});
+				});
+			}
 			socket.on('joinChannel',(joinChannelData) => {
 				socket.join(joinChannelData.channel);
-				socket.emit('addedToChannel',{channel: joinChannelData.channel});
+				socket.emit('addedToChannel', {
+					channel: joinChannelData.channel
+				});
 			});
 			socket.on('leaveChannel',(removeChannelData) => {
 				socket.leave(removeChannelData.channel);
-				socket.emit('removedFromChannel',{channel: removeChannelData.channel});
+				socket.emit('removedFromChannel', {
+					channel: removeChannelData.channel
+				});
 			});
-			socket.on('message',(data) => {
-				socket.broadcast.emit('newMessage',data);
-			});
+		});
+		socket.on('message',(data) => {
+			socket.broadcast.emit('newMessage',data);
 		});
 	});
 }
